ddc299f82b7134b024577a37b31f703107dc2ac0
thinhdinh25 <75488819+thinhdinh25@users.noreply.github.com>
2025-10-14 22:34:14 +0700

    Add example FXML and update img via code

--- README.md
+++
@@ -1,3 +0,0 @@
-# Arkanoid_OOP
-The game for learning OOP   
-JDK: https://www.azul.com/downloads/?package=jdk#downloads-table-zulu (version 25)
--- src/main/java/uet/arkanoid/App.java
+++ src/main/java/uet/arkanoid/App.java
@@ -2,44 +2,55 @@ package uet.arkanoid;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class App extends Application {
 
     @Override
-    public void start(Stage stage) {
+    public void start(Stage stage) throws IOException {
         // Kích thước cửa sổ
         double screenWidth = Gameconfig.screen_width;
         double screenHeight = Gameconfig.screen_height;
 
-        Pane pane = new Pane();
-        Scene scene = new Scene(pane, screenWidth, screenHeight);
-        List<Brick> bricks = new ArrayList<>();
-        Brick brick = new Brick(Gameconfig.screen_width / 2, 100, pane, 3);
-        bricks.add(brick);
-        Ball ball = new Ball(Gameconfig.screen_width / 2, 2, pane, screenWidth, screenHeight);
+        // Pane pane = new Pane();
+        // Scene scene = new Scene(pane, screenWidth, screenHeight);
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("map1.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        MyController controller = loader.getController();
+        controller.updateAllClass1Images();
+        // List<Brick> bricks = new ArrayList<>();
+        // Brick brick = new Brick(Gameconfig.screen_width / 2, 100, pane, 3);
+        // bricks.add(brick);
+        // Ball ball = new Ball(Gameconfig.screen_width / 2, 2, pane, screenWidth, screenHeight);
 
-        // Tạo paddle ở giữa màn hình
-        Paddle paddle = new Paddle(
-                screenWidth / 2 - Gameconfig.width_paddle / 2,
-                screenHeight - 50,
-                pane,
-                screenWidth);
+        // // Tạo paddle ở giữa màn hình
+        // Paddle paddle = new Paddle(
+        //         screenWidth / 2 - Gameconfig.width_paddle / 2,
+        //         screenHeight - 50,
+        //         pane,
+        //         screenWidth);
 
-        // Game loop đơn giản để update liên tục
-        new AnimationTimer() {
-            @Override
-            public void handle(long now) {
-                ball.update();
-                Collision.checkPaddleCollision(ball, paddle);
-                Collision.checkBrickCollision(ball, bricks);
-                HandleInput.check_input(paddle, ball, scene);
-            }
-        }.start();
+        // // Game loop đơn giản để update liên tục
+        // new AnimationTimer() {
+        //     @Override
+        //     public void handle(long now) {
+        //         ball.update();
+        //         Collision.checkPaddleCollision(ball, paddle);
+        //         Collision.checkBrickCollision(ball, bricks);
+        //         HandleInput.check_input(paddle, ball, scene);
+        //     }
+        // }.start();
+
+
 
         stage.setScene(scene);
         stage.setTitle("Test Paddle");
